d9b54e1940e1cc32a83eedcf017372772ea39479
Daniel Harding <dharding@gmail.com>
2012-05-27 15:31:41 +0100

    Make path handling cross-platform
    This change updates code that assumes the path separator is always '/',
    since that assumption is not true on Windows.  A few other minor
    pathname handling cleanups were made as well, including fixes for some
    unlikely but theoretical corner cases such as a file name starting with
    more than one dot or ending with a dot.  One result of this is that any
    non-absolute pathname passed as the name argument to the Dotfile
    constructor must now explicitly begin with a dot, whereas previously
    the dot would be assumed if not present. (Explicit is better than
    implicit.)

--- dotfiles/core.py
+++ dotfiles/core.py
@@ -25,15 +25,15 @@ else:
         os.symlink(source, link_name, os.path.isdir(source))
 
 
+SEPARATORS = os.sep + (os.altsep if os.altsep else '')
+
+
 class Dotfile(object):
 
     def __init__(self, name, target, home):
-        if name.startswith('/'):
-            self.name = name
-        else:
-            self.name = home + '/.%s' % name.strip('.')
+        self.name = os.path.join(home, name)
         self.basename = os.path.basename(self.name)
-        self.target = target.rstrip('/')
+        self.target = target.rstrip(SEPARATORS)
         self.status = ''
         if not os.path.lexists(self.name):
             self.status = 'missing'
@@ -72,7 +72,7 @@ class Dotfile(object):
         shutil.move(self.target, self.name)
 
     def __str__(self):
-        return '%-18s %-s' % (self.name.split('/')[-1], self.status)
+        return '%-18s %-s' % (self.basename, self.status)
 
 
 class Dotfiles(object):
@@ -102,7 +102,7 @@ class Dotfiles(object):
                     fnmatch.filter(all_repofiles, pat))
 
         for dotfile in repofiles_to_symlink:
-            self.dotfiles.append(Dotfile(dotfile[len(self.prefix):],
+            self.dotfiles.append(Dotfile('.' + dotfile[len(self.prefix):],
                 os.path.join(self.repository, dotfile), self.homedir))
 
         for dotfile in self.externals.keys():
@@ -114,7 +114,7 @@ class Dotfiles(object):
         """Return the fully qualified path to a dotfile."""
 
         return os.path.join(self.repository,
-                            self.prefix + os.path.basename(dotfile).strip('.'))
+                            self.prefix + os.path.basename(dotfile)[1:])
 
     def list(self, verbose=True):
         """List the contents of this repository."""
@@ -148,7 +148,7 @@ class Dotfiles(object):
 
     def _perform_action(self, action, files):
         for file in files:
-            file = file.rstrip('/')
+            file = file.rstrip(SEPARATORS)
             if os.path.basename(file).startswith('.'):
                 getattr(Dotfile(file, self._fqpn(file), self.homedir), action)()
             else:
